a2ad543544acc29e2a373edfada13c710a0c9ddc
Giancarlo <giancarlolmile@gmail.com>
2025-07-02 23:37:04 -0500

    kafka

--- src/main/java/com/mitocode/customer_service/service/TestService.java
+++ src/main/java/com/mitocode/customer_service/service/TestService.java
@@ -2,15 +2,19 @@ package com.mitocode.customer_service.service;
 
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.retry.annotation.Retry;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.Random;
 
 @Service
+@RequiredArgsConstructor
 public class TestService {
 
     private Random random = new Random();
+    private final ErrorProducerService errorProducerService;
+
 
     public String getName() {
         return "Hola";
@@ -28,6 +32,14 @@ public class TestService {
 
     public String fallback(Throwable t) {
         // Aqui se puede colocar cualquier otra acción como un servicio Async
+        errorProducerService.sendCustomErrorMessage(
+                "/test",
+                "GET",
+                500,
+                "Error processing user: " + t.getMessage(),
+                String.format("{\"test\":,\"operation\":\"testOperation\"}")
+        );
+
         return "Acciones a considerar";
     }
 }
